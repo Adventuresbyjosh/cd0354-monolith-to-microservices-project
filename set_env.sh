994c5206b8e6350e318457ba3e72c8c774af5dce
josh <josh@Phoenix-Enterprise.localdomain>
2022-08-30 23:02:10 +0100

    made modifications to docker files and refactored monolith to mircoservices api

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=udagramdb
+export POSTGRES_PASSWORD=Exploits
+export POSTGRES_HOST=udagramdb4.cnm9is4dpuzd.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=udagram-bk3
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=testing
+export JWT_SECRET=hello
 export URL=http://localhost:8100
